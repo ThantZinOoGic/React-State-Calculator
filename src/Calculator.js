95592e4f00f2d04e8de0f6f0f6342d69134407e1
ThantZinOoGic <thantzinoo@gicjp.com>
2024-02-22 13:09:49 +0630

    add title and logo

--- src/Calculator.js
+++ src/Calculator.js
@@ -17,6 +17,7 @@ export default function Calculator() {
                      className="form-control" 
                      id="exampleInputEmail1" 
                      aria-describedby="emailHelp"
+                     value={firstNum}
                      onChange={(e)=> setFirstNum(e.target.value)}
                      />
             </div>
@@ -25,11 +26,12 @@ export default function Calculator() {
               <input type="number" 
                      className="form-control" 
                      id="exampleInputPassword1"
+                     value={secondNum}
                      onChange={(e)=> setSecondNum(e.target.value)}
                      />
             </div>
             <div className="d-grid">
-            <button type="submit" 
+            <button type="button" 
                     className="btn btn-primary"
                     onClick={addHandler}
                     >Add</button>
